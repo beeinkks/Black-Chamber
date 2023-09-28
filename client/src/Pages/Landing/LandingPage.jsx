496d31128ffeaed41f9d6c14006f2a04d5338b87
Rachel N. Biesiedzinski <85037264+beeinkks@users.noreply.github.com>
2023-09-28 14:18:31 -0400

    adjusted public folder for photos/ finished home

--- client/src/Pages/Landing/LandingPage.jsx
+++ client/src/Pages/Landing/LandingPage.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import Nav from "./NavBar.js";
 import "./landing.css";
 
+import Logo from "./../Images/lightLogo.png";
+
 export default function LandingPage () {
     return (
         <div className="landing">
             <Nav />
-            <div>
-
+            <div className="landing-body">
+                <img src={Logo} className="landing-img" alt="green cube on side" />
+                
+                <h1>Cracking the Code with Your Black Chamber</h1>
             </div>
         </div>
     )
